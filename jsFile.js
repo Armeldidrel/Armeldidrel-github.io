c430a4ba9e61571912b937ad420878d49d998a81
Armeldidrel <135180256+Armeldidrel@users.noreply.github.com>
2023-06-01 01:19:48 +0200

    Add files via upload

---
+++ jsFile.js
@@ -0,0 +1,5 @@
+var the_box= document.querySelector("form input");
+var voir_le_cv = document.querySelector(".box form #btn_valider");
+voir_le_cv.addEventListener("click",()=>{
+    the_box.style.visibility="hidden"; 
+})
